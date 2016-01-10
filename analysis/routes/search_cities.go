d567ec5aeeb2fc9d5e107783703dcb2de7282d02
Adam Shannon <adam@ashannon.us>
2016-01-10 15:59:21 -0600

    analysis: re-work city search to find state for each city

--- analysis/routes/search_cities.go
+++ analysis/routes/search_cities.go
@@ -8,20 +8,39 @@ import (
 	"github.com/adamdecaf/horizon/storage"
 )
 
+type CityState struct {
+	City storage.City `json:"city"`
+	State storage.State `json:"state"`
+}
+
 type CityResult struct {
-	Cities []storage.City `json:"cities"`
+	CityStates []CityState `json:"cities"`
 }
 
 func SearchCities(w http.ResponseWriter, r *http.Request) {
+	var results []CityState
+
 	query := r.URL.Query().Get("q")
 	if query != "" {
 		cities, err := storage.SearchCitiesByName(query)
 		if err != nil {
 			fmt.Printf("[Analysis] error getting cities param='%s' err='%s'\n", query, err)
 			w.WriteHeader(503)
+			return
 		}
 
-		res, err := json.Marshal(CityResult{cities})
+		for i := range cities {
+			state, err := find_state(cities[i].StateId)
+			if err != nil {
+				fmt.Printf("[Analysis] error finding state from cache param='%s' err='%s'\n", query, err)
+				w.WriteHeader(503)
+				return
+			}
+
+			results = append(results, CityState{cities[i], state})
+		}
+
+		res, err := json.Marshal(CityResult{results})
 		if err != nil {
 			fmt.Printf("[Analysis] error marshalling cities param='%s' err='%s'\n", query, err)
 			w.WriteHeader(503)
@@ -32,4 +51,17 @@ func SearchCities(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.WriteHeader(204)
 	}
+}
+
+func find_state(state_id string) (storage.State, error) {
+	state, err := storage.FindStateById(state_id)
+	if err != nil {
+		return storage.State{}, err
+	}
+
+	if state != nil {
+		return *state, nil
+	}
+
+	return storage.State{}, fmt.Errorf("unable to really find state... (state_id=%s)", state_id)
 }

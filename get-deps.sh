40ff44bbef7961bc087464a21449b03c3b296fab
Adam Shannon <adam@ashannon.us>
2016-01-30 11:15:17 -0600

    parsing: Setup and add twitter parsing methods

--- get-deps.sh
+++ get-deps.sh
@@ -6,3 +6,4 @@ go get github.com/jzelinskie/geddit
 go get github.com/lib/pq
 go get github.com/rubenv/sql-migrate
 go get github.com/satori/go.uuid
+go get github.com/mvdan/xurls
---
+++ parsing/url_parser.go
@@ -0,0 +1,14 @@
+package parsing
+
+import (
+	"github.com/mvdan/xurls"
+)
+
+type UrlMultiParser struct {
+	MultiParser
+}
+
+func (p UrlMultiParser) Parse(input string) ([]string, error) {
+	parsed := xurls.Relaxed.FindAllString(input, -1)
+	return parsed, nil
+}

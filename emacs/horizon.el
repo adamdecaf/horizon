610850f6d113e2115478c1f7ea6f4f0d3f0284b1
Adam Shannon <adam@ashannon.us>
2016-02-19 14:28:03 -0600

    Move *.el files into emacs

--- emacs/horizon.el
+++ emacs/horizon.el
@@ -2,6 +2,6 @@
 
 (defun horizon/connect-postgres()
   (interactive)
-  (mine/sql 'postgres "horizon" "e06b4ed2b382f68" "192.168.99.100" "horizon" "."))
+  (mine/sql 'postgres "horizon" "e06b4ed2b382f68" "192.168.99.100" "horizon" ""))
 
 (provide 'horizon-utils)

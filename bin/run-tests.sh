2c6c1e2a971490f13d3a435f82d9dcfe1f10512c
Adam Shannon <adam@ashannon.us>
2016-02-22 18:33:45 -0600

    Fixing psql config for tests

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 ## Storage
 export STORAGE_USER=horizon
-export STORAGE_USERNAME=192.168.59.103
+export STORAGE_HOSTNAME=192.168.99.100
 export STORAGE_PASSWORD=e06b4ed2b382f68
+export STORAGE_PORT=5432
 
 ## Start the postgres image
 docker-compose up -d postgres 2> /dev/null

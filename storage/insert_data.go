6aa3dd0302b03c08b924a4849bd91d443190058f
Adam Shannon <adam@ashannon.us>
2016-02-26 08:51:50 -0600

    storage: \n some printfs

--- storage/insert_data.go
+++ storage/insert_data.go
@@ -83,10 +83,11 @@ func insert_gzipped_sql() {
 	fmt.Printf("[storage] Inserting gzipped sql files\n")
 
 	if run := os.Getenv("INSERT_HOSTNAMES"); run == "yes" {
+                fmt.Printf("[storage] Starting to insert 1m hostnames in .sql file\n")
 		rows, err := ExecuteGzippedSQL("storage/raw-data/top-1m-hostnames.sql.gz")
 		if err != nil {
-			fmt.Printf("[storage] Error when inserting top 1m hostnames from .sql")
+			fmt.Printf("[storage] Error when inserting top 1m hostnames from .sql err=%s\n", err)
 		}
-		fmt.Printf("[storage] Inserted %d hostname rows.", rows)
+		fmt.Printf("[storage] Inserted %d hostname rows.\n", rows)
 	}
 }
